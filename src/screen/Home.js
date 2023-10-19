f9b5682896ecdf8bc1f47343fcd1b08350d408cb
kdg2k2 <kdg2k2@gmail.com>
2023-10-19 07:49:15 +0700

    fix lỗi cuộn màn hình chính

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -39,91 +39,89 @@ const Home = ({navigation}) => {
   const {width} = Dimensions.get('window');
 
   return (
-    <View style={{backgroundColor: '#000'}}>
-      <View>
-        <Text
-          style={{
-            color: '#fafafa',
-            fontSize: 20,
-            fontWeight: 'bold',
-            marginTop: 10,
-          }}>
-          Truyện hot
-        </Text>
-        <SwiperFlatList
-          style={{
-            zIndex: 10,
-          }}
-          autoplay
-          autoplayDelay={3}
-          autoplayLoop
-          index={1}
-          data={data.truyen_view}
-          renderItem={({item}) => (
-            <View style={{margin: 5, flex: 1}}>
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('Detail', {slug: item.slug});
-                }}>
-                <Image
-                  style={{width: 170, height: 250}}
-                  source={{
-                    uri: `http://127.0.0.1:8000/${item.path}`,
-                  }}
-                />
-                <Text
-                  style={{
-                    color: '#fafafa',
-                    textAlign: 'center',
-                    maxWidth: 170,
-                    overflow: 'hidden',
+    <FlatList
+      style={{flex: 1, backgroundColor: '#000'}}
+      ListHeaderComponent={
+        <>
+          <Text
+            style={{
+              color: '#fafafa',
+              fontSize: 20,
+              fontWeight: 'bold',
+              marginTop: 10,
+            }}>
+            Truyện hot
+          </Text>
+          <SwiperFlatList
+            style={{
+              zIndex: 10,
+            }}
+            autoplay
+            autoplayDelay={3}
+            autoplayLoop
+            index={1}
+            data={data.truyen_view}
+            renderItem={({item}) => (
+              <View style={{margin: 5, flex: 1}}>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('Detail', {slug: item.slug});
                   }}>
-                  {item.tentruyen}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          )}></SwiperFlatList>
-      </View>
-
-      <View>
-        <Text
-          style={{
-            color: '#fafafa',
-            fontSize: 20,
-            fontWeight: 'bold',
-          }}>
-          Truyện mới cập nhật
-        </Text>
-        <FlatList
-          style={{backgroundColor: '#000'}}
-          data={data.truyen_update}
-          numColumns={2}
-          renderItem={({item}) => (
-            <View style={{margin: 5, flex: 1}}>
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('Detail', {slug: item.slug});
-                }}>
-                <Image
-                  style={{width: 170, height: 250}}
-                  source={{
-                    uri: `http://127.0.0.1:8000/${item.path}`,
-                  }}
-                />
-                <Text style={{color: '#fafafa', textAlign: 'center'}}>
-                  {item.tentruyen}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          )}
-          keyExtractor={item => item.id.toString()}
-          ListEmptyComponent={<Text>No data</Text>}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
-          }
-        />
-      </View>
-    </View>
+                  <Image
+                    style={{width: 170, height: 250}}
+                    source={{
+                      uri: `http://127.0.0.1:8000/${item.path}`,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      color: '#fafafa',
+                      textAlign: 'center',
+                      maxWidth: 170,
+                      overflow: 'hidden',
+                    }}>
+                    {item.tentruyen}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+          <Text
+            style={{
+              color: '#fafafa',
+              fontSize: 20,
+              fontWeight: 'bold',
+            }}>
+            Truyện mới cập nhật
+          </Text>
+        </>
+      }
+      data={data.truyen_update}
+      numColumns={2}
+      renderItem={({item}) => (
+        <View style={{margin: 5, flex: 1}}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Detail', {slug: item.slug});
+            }}>
+            <Image
+              style={{width: 170, height: 250}}
+              source={{
+                uri: `http://127.0.0.1:8000/${item.path}`,
+              }}
+            />
+            <Text style={{color: '#fafafa', textAlign: 'center'}}>
+              {item.tentruyen}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      keyExtractor={item => item.id.toString()}
+      ListEmptyComponent={<Text>No data</Text>}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+      }
+    />
   );
 };
 export default Home;
